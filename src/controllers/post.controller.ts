1958a53d4969c6fe5f1d3462130bacc73c5d17d4
AlekCole <note.boonanan@gmail.com>
2024-04-28 13:52:21 +0700

    Fix controller posts

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -4,22 +4,18 @@ import prisma from '../client';
 prisma.$connect();
 
 const createBlogPost = async (req: Request, res: Response) => {
-  console.log("req");
-  res.status(200).json(req.body);
-  // try {
-    
-    
-  //   const { title, content } = req.body;
-  //   const newBlogPost = await prisma.post.create({
-  //     data: {
-  //       title,
-  //       content,
-  //     },
-  //   });
-  //   res.status(200).json(newBlogPost);
-  // } catch (e) {
-  //   res.status(500).json({ error: e });
-  // }
+  try {
+    const { title, content } = req.body;
+    const newBlogPost = await prisma.post.create({
+      data: {
+        title,
+        content,
+      },
+    });
+    res.status(200).json(newBlogPost);
+  } catch (e) {
+    res.status(500).json({ error: e });
+  }
 }
 
 const getBlogPosts = async (req: Request, res: Response) => {
